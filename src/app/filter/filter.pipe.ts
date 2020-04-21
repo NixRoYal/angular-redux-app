5bc603dd9e9c14a2852ad28ccdadca29d89b5fcd
NixRoYal <hdxtremo@gmail.com>
2020-04-20 20:18:28 -0500

    Aplicación de produción lista

--- src/app/filter/filter.pipe.ts
+++ src/app/filter/filter.pipe.ts
@@ -1,12 +1,10 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { Todo } from '../todo/models/todo.model';
 import * as fromFiltro from "./filter.actions";
-@Pipe({
-  name: 'filter'
-})
+@Pipe({ name: 'filterDt' })
 export class FilterPipe implements PipeTransform {
 
-  transform(todos: Todo[], filtro: fromFiltro.filtrosValidos): Todo[] {
+  transform(todos: Todo[], filtro: String): Todo[] {
     switch (filtro) {
       case 'completados': return todos.filter(td => td.completado);
       case 'pendientes': return todos.filter(td => !td.completado);
--- src/app/todo/todos-list/todos-list.component.ts
+++ src/app/todo/todos-list/todos-list.component.ts
@@ -3,23 +3,15 @@ import { Store } from '@ngrx/store';
 import { AppState } from '../../app.reducers';
 import { Todo } from '../models/todo.model';
 
-@Component({
-  selector: 'app-todos-list',
-  templateUrl: './todos-list.component.html',
-  styles: []
-})
+@Component({ selector: 'app-todos-list', templateUrl: './todos-list.component.html' })
 export class TodosListComponent implements OnInit {
-
   todos: Todo[] = [];
   filtro: string;
-
   constructor(private store: Store<AppState>) { }
-
   ngOnInit() {
     this.store.subscribe(state => {
       this.todos = state.todos;
       this.filtro = state.filtro;
     });
   }
-
 }
